bf0fe1d0d4ab1be7466cc868f3ad63c7a3a55265
Santina <s@croniser.com>
2016-02-25 23:05:30 -0600

    add fibonacci

---
+++ fibonacci.rb
@@ -0,0 +1,16 @@
+def fibonacci(n)
+  arr = []
+  if n==1
+    arr.push(1)
+  else
+    arr.push(1,1)
+    (3..n).each do |x|
+      arr.push(arr[x-3] + arr[x-2])
+    end
+
+  end
+  return arr
+end
+
+#test
+#print fibonacci(7)
